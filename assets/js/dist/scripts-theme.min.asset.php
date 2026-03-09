4cbefefb6b9b956782a7b1b58e78a34a5e953306
fabiomsnunes <fabiomsnunes@hotmail.com>
2026-03-09 22:12:45 +0000

    Update to 2.9.28.1

--- assets/js/dist/scripts-theme.min.asset.php
+++ assets/js/dist/scripts-theme.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('gform_gravityforms_utils', 'jquery', 'wp-polyfill'), 'version' => '0d0b3e212e71eb03f349');
+<?php return array('dependencies' => array('gform_gravityforms_utils', 'jquery', 'wp-polyfill'), 'version' => 'd5bd96a108425385b437');
